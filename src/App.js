f35d27021f479a45d955765724a7806de10a4819
Елена Забудская <zabudskaya@MacBook-Pro-Elena.local>
2019-06-01 19:00:19 +0300

    modified App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const text = "Test № ";
+const  boolean = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
+        <h1 style={{
+            color: 'red'
+        }}>Hello World!</h1>
+          <p>
+              {text}
+              {1}
+          </p>
+          <p>
+              {text}
+              {1+1}
+          </p>
+          <p>
+              {boolean ? 'boolean is true' : 'boolean is false'}
+          </p>
+          <p>
+              {boolean && 'boolean is true'}
+          </p>
+          <p>
+              {undefined}
+              {null}
+              {false}
+              {true}
+          </p>
       </header>
     </div>
   );
