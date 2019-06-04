d0ce609c14bb458d9293c65ab615c629239451c1
Елена Забудская <zabudskaya@MacBook-Pro-Elena.local>
2019-06-04 13:47:20 +0300

    create component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const el = (
-    <h1>
-      Hello World!
-    </h1>
+const  ItemList = () => (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+    </ul>
 );
-export { el };
+const App = () => ( <div>
+  <h1>Hello World!</h1>
+  <ItemList/>
+</div>);
+
+export default App;
